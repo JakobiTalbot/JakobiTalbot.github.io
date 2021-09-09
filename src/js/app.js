ed538c005f1467021aa192b7acafc200d37ba6c2
Jakobi Talbot <jakobi.talbot@gmail.com>
2021-09-09 19:00:05 +1000

    slower particles

--- src/js/app.js
+++ src/js/app.js
@@ -59,7 +59,7 @@ document.addEventListener("DOMContentLoaded", function () {
       },
       "move": {
         "enable": true,
-        "speed": 4,
+        "speed": 0.5,
         "direction": "none",
         "random": true,
         "straight": false,
